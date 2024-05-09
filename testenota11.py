4fac19f1168d8dfb6495ba31fdd898dd7f94afd5
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2024-05-09 12:29:34 -0300

    novatualizacao

---
+++ testenota11.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import os.path
+import json
+import requests
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.nome} has a :{row.nota}:")
