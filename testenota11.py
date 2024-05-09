9ecc948ae86a27a8d4614e219c997d92bd009728
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2024-05-09 12:33:16 -0300

    Update testenota11.py

--- testenota11.py
+++ testenota11.py
@@ -3,6 +3,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from streamlit_gsheets import GSheetsConnection
 from googleapiclient.errors import HttpError
 import os.path
 import json
