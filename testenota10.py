829e25caf0438162f37a7870590bc61767742de3
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2024-05-09 13:40:16 +0000

    atualizacao do codigo

--- testenota10.py
+++ testenota10.py
@@ -5,37 +5,46 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import os.path
+import json
+import requests
 
-
-
-
-# Carregar as credenciais do ambiente de execução (configuração específica do ambiente)
-credentials = Credentials.from_service_account_info({
-  "type": "service_account",
-  "project_id": "digitacaonotas",
-  "private_key_id": "9141920045fa5fe712e251daa42466bbd9d13c57",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC9GPab0x/EFrB4\nZyDImktqFf7rVjf3tu/qGRxJT47S8fE+nz9mCie6bfH8MqYH344+mnbR5/nyZUGy\nrWXuZ2DnJRNX9hqe8RNBnSPO5t1jRN1psecdiPJzDXqZ/mE/nGAwDfdpJLRn2PLN\nl2srNOXNShVhx2Y/c852CucGM8VKm6ZsFh9STRkQLhdOm4jdK0yfMNRmaDS1FVA3\nYNjbLpt3DOW/nDJw3kDC95jqS7+JVmF0/2HNkQnLznfaHlnQEtLrNQNL6IRunIUx\n1iateTDQavF/JN8aOfchwWFNObuqxYWOlDAm6kgB2ghM+WB4tA3RwyY9cOYBrcvv\nz8YZ3lyHAgMBAAECggEAAodaiBiGRjlsQE/wvX4CucQNiQwgHeBRmQNsJp05WOzP\nWXnVTGhtjsd4z7/FpJ+156edJMJBzDhtoBWqVUo6OFBKNlTq/Wq8sGK8X3OK5l1X\nY9oMyY8fq6/ODra68JgHJhCQJgg0BjRKgVSGPfYSuwoQlLwoske6k3pV2I/JeNPr\nvsivourYKvmabeUBzffiZ/T/CwC7/KUON1qRpcrUEZd+eF3hq14ODopLOXHdsW41\nP0nyWv6GqYGughgqIsAIUkQhxMRoRSoM62Vbs3BB5QhBENIPKRG3r9e+T9BNKIHH\nbYHWM6tgJpk7JNtrVyOkoV+XmHQd/A4kh+Ek9nxMgQKBgQDfcN6qmzdKn8liIjL1\nzIf4Rg1M1yNKHr+cMVW0TbHJLAGecdnfgkIgFvMcx1gHGgz05inktGCy0Bc9/yFn\n/jeWydeZ+T2SSGuqAB/IVVmNhK4yPNwdfBWxQzJ0Fs1YeUK7KFnC+tF0aalnUfsF\nlOVhvsF4T4hOyUxkcy/YD5JUIQKBgQDYpvb0ElCce8s8J8hbC/A4AIOD0irsDcr+\nkNsP4g8H6uTvtpSC8I9TjtJP0CSx+vECusSmiTdVdG32RQAmIZwtjgMVKwHOhVK3\nQHhbEtlCpEKAjTOdETU6UEt2v2qVqqK1G+XrCcCL+maRai3aFYc7fhXX+pyEAgMP\nB/6tEUobpwKBgBERQBDsFkce+XUDg7z+yIwQXce2zz1EKAK4SgGhxajGMcF2lNvb\nyU1GM3/JQlIYjVDPm/eaR6ChAIV9zYFyC7D67fsW7LsVyRONOuBElLiYn+26dF3U\nMpv9tAbmDv1pFc3SATlBi2pG/+eifXgprXPP7FtSR+ksuycZHVh1qAnBAoGBAJfJ\nKj1HU5i+ll231YQMMilaGsnRBedJpo2BOSjAjR1T8T8MoLG4DgJRjoECZ6MWF6J8\n7WdrvQuuvZeLMhaE96Fm3DwJXIOvHotqrKfJ5dLtt/XOd3m0Y6FOPgCdDPR8ju/p\n8sQfmfm33gJE7IjdClig9nbRirRloHT4efIQqeYDAoGAJdFSPVWr/94E4uxEcvCm\nUNwRUJ9ii7Kn99o3cuFhIYS/bwvaySg3uvXqYxX8aoEmfMFSqI9gx3mgSHLOFWN+\nxkc3w+3grhdiz8hsltmSo0rrVnl5kIP3HUmtLC1ND1Kc92xkmwQuyYR8fo1BHLKx\nAvfMvJwWGkQPSsiHEY50umI=\n-----END PRIVATE KEY-----\n",
-  "client_email": "digitarnotas@digitacaonotas.iam.gserviceaccount.com",
-  "client_id": "117384486449011450121",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/digitarnotas%40digitacaonotas.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
-
-# Definir o escopo de autorização para acessar o Google Sheets
+# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-# O ID da planilha do Google Sheets que você deseja acessar ou modificar
-SAMPLE_SPREADSHEET_ID = "1OahlvKuGODaTet_Y-OL6s82eufuYfVo6jPxSgxCxsd4"
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1S1oID1RUYCyYxq2ZRcAmY0XVx1PaO7EJTtRLNDoLLPM"
 
-# Criar o cliente do Google Sheets
-service = build("sheets", "v4", credentials=credentials)
-sheet = service.spreadsheets()
+def get_credentials():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # Carrega as credenciais do arquivo credenciaisweb.json no GitHub
+            cred_url = "https://raw.githubusercontent.com/renatdoug/notaescola/main/credenciaisweb.json"
+            response = requests.get(cred_url)
+            if response.status_code == 200:
+                creds_data = response.json()
+                creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
+            else:
+                st.error("Erro ao carregar as credenciais.")
+
+        # Salva as credenciais no arquivo 'token.json' para uso posterior
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return creds
 
 def main():
     st.title("Adicionar Nota dos Alunos")
+
+    # Autenticação e autorização do Google Sheets
+    creds = get_credentials()
+    service = build("sheets", "v4", credentials=creds)
+    sheet = service.spreadsheets()
 
     # Obter a lista de abas (nome das disciplinas) da planilha
     spreadsheet = sheet.get(spreadsheetId=SAMPLE_SPREADSHEET_ID).execute()
